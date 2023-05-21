72ef4f80c621974cbb74d9ce6aa1dd331b081689
HoangHuynh145 <95960229+HoangHuynh145@users.noreply.github.com>
2023-05-21 22:17:33 +0700

    Update UI articles profile

--- src/components/article/ProfileArticle.tsx
+++ src/components/article/ProfileArticle.tsx
@@ -17,8 +17,8 @@ const ProfileArticle = ({ ownPosts }: { ownPosts: [OwnPost] }) => {
                             <img src={renderImgSrc(post.thumbnail)} className='w-full h-full object-cover object-center' />
                         </div>
                         <div className='sm:col-span-1 flex-1 text-start text-slate-800 dark:text-slate-200'>
-                            <span className='font-medium'>{post.title}</span>
-                            <p className='text-sm mt-1 mb-3.5 text-slate-900/70 text-slate-800 dark:text-slate-200 line-clamp-4'>
+                            <span className='font-semibold line-clamp-2'>{post.title}</span>
+                            <p className='text-sm mt-1 mb-3.5 text-slate-900/70 text-slate-700 dark:text-slate-400 line-clamp-3'>
                                 {post.introContent}
                             </p>
                         </div>
